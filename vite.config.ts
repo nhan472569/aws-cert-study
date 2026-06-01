2c0ba2529bc9331503a7ee23215eacc2174cde3d
Nhan Dao <nhan472569@gmail.com>
2026-06-01 22:55:12 +0700

    Add GitHub Actions workflow for deploying to GitHub Pages and update Vite config for repository base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: process.env.GITHUB_REPOSITORY
+        ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+        : '/',
     plugins: [react(), tailwindcss()],
 });
