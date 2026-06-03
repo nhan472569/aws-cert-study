f361e92b15b0e85ff35e2d9cb3e945e752355f4a
Nhan Dao <nhan472569@gmail.com>
2026-06-03 22:37:04 +0700

    Update start_url in PWA configuration for environment-specific routing

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
                 display: 'standalone',
                 orientation: 'any',
                 scope: '/',
-                start_url: '/',
+                start_url: '/aws-cert-study',
                 categories: ['education', 'productivity'],
                 icons: [
                     {
